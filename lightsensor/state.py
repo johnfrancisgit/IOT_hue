5748be7f577d749b387553a1f9e825f7e9bdfcdc
Jonas Cosandey <jonascosandey@hotmail.com>
2017-10-25 16:02:37 +0200

    hue api calls and status reading

--- lightsensor/state.py
+++ lightsensor/state.py
@@ -1,15 +1,19 @@
 import RPi.GPIO as GPIO
-
 from time import sleep
 from termcolor import colored
 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4,GPIO.IN)
-    
-    while True:
-        message = colored('On', 'green') if GPIO.input(4) == 0 else colored('Off', 'red')
-        print(message)
-        sleep(1)
-finally:
-    GPIO.cleanup()
+class Sensor(object):
+    def setup():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4,GPIO.IN)
+
+    def get_status(debug=False):
+        if debug:
+            message = colored('On', 'green') if GPIO.input(4) == 0 else colored('Off', 'red')
+            print(message)
+            sleep(1)
+        return GPIO.input(4)
+
+    def tear_down():
+        GPIO.cleanup()
+
