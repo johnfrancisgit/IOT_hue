b562ce08b45a378d992a6349fdf55ec3b90695c6
John Francis <jfswitz@gmail.com>
2017-10-25 15:03:02 +0200

    better msg

--- lightsensor/state.py
+++ lightsensor/state.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 
 from time import sleep
+from termcolor import import colored
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
 
 while True:
-    message = True if GPIO.input(4) == 0 else False
+    message = colored('On', 'green') if GPIO.input(4) == 0 else colored('Off', 'red')
     print(message)
     sleep(1)
