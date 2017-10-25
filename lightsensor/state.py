c464a1c6803429fb2b1a9de834ebd48b15be3f2c
John Francis <jfswitz@gmail.com>
2017-10-25 15:26:05 +0200

    cleanup gpio

--- lightsensor/state.py
+++ lightsensor/state.py
@@ -3,10 +3,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 from termcolor import colored
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.IN)
-
-while True:
-    message = colored('On', 'green') if GPIO.input(4) == 0 else colored('Off', 'red')
-    print(message)
-    sleep(1)
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4,GPIO.IN)
+    
+    while True:
+        message = colored('On', 'green') if GPIO.input(4) == 0 else colored('Off', 'red')
+        print(message)
+        sleep(1)
+finally:
+    GPIO.cleanup()
